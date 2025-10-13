afef484daf88e638c21f5040222ac86f7df2791c
root <root@OVM.myguest.virtualbox.org>
2025-10-14 00:19:19 +0300

    cidr

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -9,3 +9,15 @@ output "subnet_ids" {
 output "security_group_id" {
   value = yandex_vpc_security_group.ssh.id
 }
+
+output "subnet_a_cidr" {
+  value = yandex_vpc_subnet.subnet_a.v4_cidr_blocks[0]
+}
+
+output "subnet_b_cidr" {
+  value = yandex_vpc_subnet.subnet_b.v4_cidr_blocks[0]
+}
+
+output "subnet_d_cidr" {
+  value = yandex_vpc_subnet.subnet_d.v4_cidr_blocks[0]
+}
